e73dbb48c79b8496fe96a2f7c123ab51d9500082
Rok Andrée <rok@andree.si>
2014-02-01 22:50:21 +0100

    Update to import script to function with new central data repo

--- src/PPI/ApiBundle/Command/LoadDataCommand.php
+++ src/PPI/ApiBundle/Command/LoadDataCommand.php
@@ -14,7 +14,6 @@ class LoadDataCommand extends ContainerAwareCommand
         $this
             ->setName('ppi:api:loadData')
             ->setDescription('Load JSON data into redis')
-            ->addArgument('file', InputArgument::REQUIRED, 'File to import')
         ;
     }
 
@@ -24,45 +23,55 @@ class LoadDataCommand extends ContainerAwareCommand
         $this->logger = $this->getContainer()->get('logger');
         $this->container = $this->getContainer();
 
-        $file = $input->getArgument('file');
+        $appRoot = $this->container->get('kernel')->getRootDir() . '/..';
 
         $this->log("Starting data import");
-        $data = file_get_contents($file);
-        if ($data === false) {
-        	$this->log("Could not get file contents");
-        	return;
-        }
-        if (empty($data)) {
-        	$this->log("File is empty");
-        	return;
-        }
-        $data = json_decode($data);
-        if ($data === null) {
-        	$this->log("Data could not be decoded from JSON");
-        	return;
-        }
+        define('INCLUDE_RETURN_DATA', true);
+        $data = include 'data/compile.php';
 
         $this->log("Data retrived sucessfully");
 
         $redis = $this->container->get('snc_redis.default');
 
-        $this->log(sprintf("Processing %s organisations.", count($data)));
+        $this->log(sprintf("Processing meta data for %s organisations.", count($data)));
 
-        foreach ($data as $partyKey => $partyData) {
+        foreach ($data['data'] as $partyKey => $partyData) {
         	$this->log("    - Processing " . $partyKey);
-        	if (isset($partyData->partyName->en)) {
-        		if (preg_match('/(.+) \((.+)\)/i', $partyData->partyName->en, $matches) === 1) {
-        			$partyData->partyName->en = $matches[1];
-        			$partyData->partyCode = $matches[2];
-        		}
-        	}
             $partyData->lastUpdate = date('c');
-            $partyData->id = $partyKey;
-        	$redis->set('ppi:orgs:' . $partyKey, json_encode($partyData));
+        	$redis->set('ppi:orgs:' . strtolower($partyKey), json_encode($partyData));
+        }
+
+        $this->log("Done with orgs.");
+
+        $this->log("Processing logos");
+
+        $logoDir = $appRoot . '/web/img/pp-logo/';
+        if (!is_dir($logoDir)) {
+            mkdir($logoDir, 0755, true);
+        }
+
+        foreach ($data['logo'] as $logoKey => $logoPath) {
+            preg_match('/.+\/(([a-zA-Z-]+)\.(png|jpg))$/i', $logoPath, $matches);
+            $filename = $matches[1];
+            copy($logoPath, $logoDir . $filename);
+            $redis->set('ppi:logos:' . strtolower($logoKey), '/img/pp-logo/' . $filename);
+        }
+
+        $this->log("Processing flags");
+
+        $flagDir = $appRoot . '/web/img/pp-flag/';
+        if (!is_dir($flagDir)) {
+            mkdir($flagDir, 0755, true);
+        }
+
+        foreach ($data['flag'] as $flagKey => $flagPath) {
+            preg_match('/.+\/(([a-zA-Z-]+)\.(png|jpg))$/i', $flagPath, $matches);
+            $filename = $matches[1];
+            copy($flagPath, $flagDir . $filename);
+            $redis->set('ppi:flags:' . strtolower($flagKey), '/img/pp-flag/' . $filename);
         }
 
         $this->log("Done.");
-
         
     }
 
