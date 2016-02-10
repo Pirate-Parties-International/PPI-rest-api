f1acaa11d5ba11d95fc2aef7e0c9ffb23e0c109e
Rok Andrée <rok@andree.si>
2016-02-10 18:42:45 +0100

    [#14] Frontpage now  uses new backend

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -16,33 +16,13 @@ class DefaultController extends BaseController
     public function indexAction()
     {
 
-    	$allData = $this->getAllPartiesData();
+    	$parties = $this->getAllParties();
 
-    	$output = array();
-    	foreach ($allData as $partyKey => $party) {
-
-    		$output[$partyKey] = array(
-                'label'      => $party->partyName->en,
-                'partyCode'  => $party->partyCode,
-                'link'       => '/party/' . $partyKey,
-                'apiLink'    => '/api/v1/parties/' . $partyKey,
-                'logo'       => false,
-                'membership' => [
-                    'ppeu' => isset($party->membership->ppeu) ? $party->membership->ppeu : false,
-                    'ppi' => isset($party->membership->ppi) ? $party->membership->ppi : false
-                ],
-                'type'       => $party->type
-    		); 
-    		$logo = $this->getPartyLogo($partyKey);
-    		if ($logo !== null) {
-    			$output[$partyKey]['logo'] = $logo;
-    		}
-    	}
-        return array("parties" => $output);
+        return array("parties" => $parties);
     }
 
     /**
-     * @Route("/party/{id}")
+     * @Route("party/{id}", name="papi_party_show")
      * @Template()
      */
     public function partyAction($id)
