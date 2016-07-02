201f06a05eea1c468ac7adbb55c60d5f9024c2f0
Rok Andrée <rok@andree.si>
2016-07-02 20:13:56 +0200

    [] Bugfix for importing of new IntOrg

--- src/AppBundle/Command/LoadDataCommand.php
+++ src/AppBundle/Command/LoadDataCommand.php
@@ -70,8 +70,12 @@ class LoadDataCommand extends ContainerAwareCommand
         ->getRepository('AppBundle:InternationalOrg')
         ->findOneByCode('ppi');
 
-        if (!$intOrg['ppeu'] || !$intOrg['ppi']) {
-            $this->log("Int orgs are missing - run fixtures first!");
+        $intOrg['ype'] = $this->container->get('doctrine')
+        ->getRepository('AppBundle:InternationalOrg')
+        ->findOneByCode('ype');
+
+        if (!$intOrg['ppeu'] || !$intOrg['ppi'] || !$intOrg['ype']) {
+            $this->log("Int orgs are missing!");
             return;
         }
         
