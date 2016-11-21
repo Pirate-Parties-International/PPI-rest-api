3478d4338405b3018565e9f6ebc2f802502e8f23
GuruVII <guruvii@gmail.com>
2016-11-21 16:56:16 +0100

    added an ng-rpeat which will in the end show pictures and added infite scrolling

--- web/js/pictureList.js
+++ web/js/pictureList.js
@@ -1,26 +1,56 @@
 (function($) {
-    var app = angular.module('app', []);    
-        app.controller('pictureController', ['$scope', function($scope) {
-            $scope.greeting = 'Hola!';
-            //temporary function that creates fake data in the same format as I expect to get the data though an API
-            var getFakeData = function(){
-                var data = {};           
-                for (var i = 0; i < 1000; i++){
-                    data[i] = { url: "http://loremflickr.com/200/200?random="+i};
-                    if ((i%2)==0){
-                        data[i]["socialPlatform"] = "FB"
-                        data[i]["likes"] = 10000 + (i)*2
-                    }
-                    else {
-                        data[i]["socialPlatform"] = "TW"
-                        data[i]["likes"] = 10000 + (-i)*2
-                    };
+    var app = angular.module('app', ['infinite-scroll']); 
+
+       
+    app.controller('pictureController', ['$scope', '$timeout', function($scope, $timeout) {
+        $scope.greeting = 'Hola!';
+        //temporary function that creates fake data in the same format as I expect to get the data though an API
+        var getFakeData = function(){
+            var data = {};           
+            for (var i = 0; i < 1000; i++){
+                data[i] = { url: "http://loremflickr.com/200/200?random="+i};
+                if ((i%2)==0){
+                    data[i]["socialPlatform"] = "FB"
+                    data[i]["likes"] = 10000 + (i)*2
+                }
+                else {
+                    data[i]["socialPlatform"] = "TW"
+                    data[i]["likes"] = 10000 + (-i)*2
                 };
-                return data;
-        	};
-            //Function that runs after data is acquired
-            $.when(getFakeData()).done(function(data){ 
-                console.log(data)
+            };
+            return data;
+    	};
+        //this functions loads more data for the infinite scroll
+        $scope.loadMore = function() {
+            var last = $scope.data[$scope.data.length - 1];
+            for(var i = 1; i <= 20; i++) {
+              $scope.data.push($scope.tempData[last + i]);
+            }
+            $scope.fireEvent()
+        };
+        // This will only run after the ng-repeat has rendered its things to the DOM
+        // there is a slighly timeout, because ng-repeat doesn't render fast enough
+        //this function wraps every 4 images into its own row
+        $scope.fireEvent = function(){
+            $timeout(function(){
+                var classes = $(".s3.placement-class");
+                console.log(classes)
+                for(var i = 0; i < classes.length; i+=4) {
+                classes.slice(i, i+4).wrapAll("<div class='row'></div>");
+                }
+            }, 50)
+        };  
+
+        //Function that runs after data is acquired
+        $.when(getFakeData()).done(function(data){ 
+            $scope.tempData =[];
+            $scope.data =[];
+            for (var x in data){
+                $scope.tempData.push(data[x]);
+            };
+            $scope.loadMore();   
         });
-   }]);
+
+
+    }]);
 })(jQuery); // end of jQuery name space
