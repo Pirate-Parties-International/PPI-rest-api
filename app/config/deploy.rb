206812dfddd51de23a422d5b6aabcbe6206d97c9
Rok Andrée <rok@andree.si>
2017-01-07 14:11:46 +0100

    [] Fixes deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -30,11 +30,12 @@ set :permission_method,   :acl
 set :use_set_permissions, true
 
 set :shared_files,      ["app/config/parameters.yml"]
-set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor", 
+set :shared_children,   [app_path + "/logs", "vendor", 
 	"app/Resources/java", "etc", 
 	web_path + "/img/pp-flag",
 	web_path + "/img/pp-logo",
-	web_path + "/img/fb-covers"]
+	web_path + "/img/fb-covers",
+  web_path + "/img/uploads"]
 
 set :stages,        %w(production dev)
 set :default_stage, "dev"
